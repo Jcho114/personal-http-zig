338d3f347b85d23487624d99da7e47c8e7ff7455
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2025-03-16 20:52:20 -0400

    chore: converted parse function to struct method

--- src/server.zig
+++ src/server.zig
@@ -16,36 +16,11 @@ pub fn handleConnection(conn: std.net.Server.Connection) !void {
         buffer[i] = 0;
     }
     const numBytes = try conn.stream.read(&buffer);
-    var request = try parseRequest(buffer[0..numBytes]);
+    var request = try Request.parse(buffer[0..numBytes]);
     defer request.deinit();
     std.debug.print("Target: {s}\n", .{request.target});
     std.debug.print("Host: {s}\n", .{request.headers.get("Host") orelse "null"});
     std.debug.print("Body: {s}\n", .{request.body});
-}
-
-pub fn parseRequest(stream: []u8) !Request {
-    var it = std.mem.splitSequence(u8, stream, "\n");
-    const firstLine = it.next() orelse return error.SomeError;
-    var firstLineIt = std.mem.splitSequence(u8, firstLine, " ");
-    const methodString = firstLineIt.next() orelse return error.SomeError;
-    const method = std.meta.stringToEnum(Method, methodString) orelse return error.SomeError;
-    const target = firstLineIt.next() orelse return error.SomeError;
-    const protocol = firstLineIt.next() orelse return error.SomeError;
-
-    var headers = Headers.init(std.heap.page_allocator);
-    while (it.next()) |header| {
-        if (header.len == 1) break;
-        var headerIt = std.mem.splitSequence(u8, header, ": ");
-        const key = headerIt.next() orelse return error.SomeError;
-        const value = headerIt.rest();
-        std.debug.print("{s}: {s}\n", .{ key, value });
-        try headers.put(key, value);
-    }
-
-    const body = it.rest();
-
-    const request: Request = .{ .method = method, .target = target, .protocol = protocol, .headers = headers, .body = body };
-    return request;
 }
 
 const Method = enum { GET, POST, PUT, DELETE };
@@ -58,6 +33,30 @@ const Request = struct {
     protocol: []const u8,
     headers: Headers,
     body: []const u8,
+
+    pub fn parse(stream: []u8) !Request {
+        var it = std.mem.splitSequence(u8, stream, "\n");
+        const firstLine = it.next() orelse return error.SomeError;
+        var firstLineIt = std.mem.splitSequence(u8, firstLine, " ");
+        const methodString = firstLineIt.next() orelse return error.SomeError;
+        const method = std.meta.stringToEnum(Method, methodString) orelse return error.SomeError;
+        const target = firstLineIt.next() orelse return error.SomeError;
+        const protocol = firstLineIt.next() orelse return error.SomeError;
+
+        var headers = Headers.init(std.heap.page_allocator);
+        while (it.next()) |header| {
+            if (header.len == 1) break;
+            var headerIt = std.mem.splitSequence(u8, header, ": ");
+            const key = headerIt.next() orelse return error.SomeError;
+            const value = headerIt.rest();
+            try headers.put(key, value);
+        }
+
+        const body = it.rest();
+
+        const request: Request = .{ .method = method, .target = target, .protocol = protocol, .headers = headers, .body = body };
+        return request;
+    }
 
     pub fn deinit(self: *Request) void {
         self.headers.deinit();
