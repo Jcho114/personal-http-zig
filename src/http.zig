f5e197060f032d9ba67c9bf9df1f183102966358
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2025-03-17 02:46:38 -0400

    feat: abstracted http client and server and also added better socket reads (no underreads)

--- src/http.zig
+++ src/http.zig
@@ -1,5 +1,141 @@
 const std = @import("std");
 
+pub const HttpClient = struct {
+    address: std.net.Address,
+    buffer: [1000]u8 = undefined,
+
+    pub fn init(host: []const u8, port: u16) !*HttpClient {
+        const address = try std.net.Address.parseIp4(host, port);
+        const httpClient = try std.heap.page_allocator.create(HttpClient);
+        httpClient.* = .{ .address = address };
+        return httpClient;
+    }
+
+    pub fn send(self: *HttpClient, request: *Request) !*Response {
+        var stream = try std.net.tcpConnectToAddress(self.address);
+        const formatted = try std.fmt.bufPrint(&self.buffer, "{}", .{request});
+        _ = try stream.writeAll(formatted);
+
+        const totalRead = try readHttp(&self.buffer, stream);
+
+        const response = try Response.parse(self.buffer[0..totalRead]);
+        return response;
+    }
+
+    pub fn deinit(self: *HttpClient) void {
+        std.heap.page_allocator.delete(self);
+    }
+};
+
+const Handler = fn (*Request, *Response) anyerror!void;
+const Routes = std.StringHashMap(*const Handler);
+
+pub const HttpServer = struct {
+    server: std.net.Server,
+    routes: Routes,
+    buffer: [1000]u8 = undefined,
+
+    pub fn init(port: u16) !*HttpServer {
+        const address = try std.net.Address.parseIp4("127.0.0.1", port);
+        const server = try address.listen(.{});
+        const httpServer = try std.heap.page_allocator.create(HttpServer);
+        httpServer.* = .{ .server = server, .routes = Routes.init(std.heap.page_allocator) };
+        return httpServer;
+    }
+
+    pub fn deinit(self: *HttpServer) void {
+        self.server.deinit();
+        self.routes.deinit();
+        std.heap.page_allocator.destroy(self);
+    }
+
+    pub fn run(self: *HttpServer) !void {
+        while (true) {
+            const conn = try self.server.accept();
+            try self.handleConnection(conn);
+        }
+    }
+
+    fn handleConnection(self: *HttpServer, conn: std.net.Server.Connection) !void {
+        defer conn.stream.close();
+        const totalRead = try readHttp(&self.buffer, conn.stream);
+        var request = try Request.parse(self.buffer[0..totalRead]);
+        defer request.deinit();
+        std.debug.print("Request\n{}\n\n", .{request});
+
+        const handler = self.routes.get(request.target) orelse defaultHandler;
+        const response = try Response.init();
+        defer response.deinit();
+        try handler(request, response);
+        std.debug.print("Response\n{}\n\n", .{response});
+
+        const formatted = try std.fmt.bufPrint(&self.buffer, "{}", .{response});
+        _ = try conn.stream.writeAll(formatted);
+    }
+
+    pub fn route(self: *HttpServer, target: []const u8, handler: Handler) !void {
+        try self.routes.put(target, handler);
+    }
+};
+
+fn readHttp(buffer: []u8, stream: std.net.Stream) !usize {
+    var totalRead: usize = 0;
+    while (true) {
+        const bytesRead = try stream.read(buffer[totalRead..]);
+        if (bytesRead == 0) break;
+        totalRead += bytesRead;
+        if (totalRead >= buffer.len) {
+            std.debug.print("Buffer too small for response\n", .{});
+            break;
+        }
+        if (std.mem.count(u8, buffer[0..totalRead], "\r\n\r\n") == 1) {
+            break;
+        }
+    }
+
+    const headerEnd = std.mem.indexOf(u8, buffer[0..totalRead], "\r\n\r\n") orelse return totalRead;
+    const headers = buffer[0..headerEnd];
+    var bodyLen: usize = 0;
+    var headerIter = std.mem.splitSequence(u8, headers, "\r\n");
+
+    while (headerIter.next()) |line| {
+        if (std.mem.startsWith(u8, line, "Content-Length: ")) {
+            const lengthStr = std.mem.trimLeft(u8, line[16..], " ");
+            bodyLen = try std.fmt.parseInt(usize, lengthStr, 10);
+            break;
+        }
+    }
+
+    const bodyStart = headerEnd + 4;
+    const alreadyReadBody = if (totalRead > bodyStart) totalRead - bodyStart else 0;
+
+    var bodyRead = alreadyReadBody;
+    while (bodyRead < bodyLen) {
+        if (bodyRead >= bodyLen) {
+            break;
+        }
+
+        if (totalRead >= buffer.len) {
+            std.debug.print("Buffer too small for response\n", .{});
+            break;
+        }
+
+        const bytesRead = try stream.read(buffer[totalRead..]);
+        if (bytesRead == 0) break;
+        bodyRead += bytesRead;
+        totalRead += bytesRead;
+    }
+
+    return totalRead;
+}
+
+fn defaultHandler(_: *Request, response: *Response) !void {
+    response.* = .{
+        .statusCode = 404,
+        .statusText = "Not Found",
+    };
+}
+
 pub const Method = enum { GET, POST, PUT, DELETE };
 
 pub const Headers = std.StringHashMap([]const u8);
@@ -7,12 +143,17 @@ pub const Headers = std.StringHashMap([]const u8);
 pub const Request = struct {
     method: Method,
     target: []const u8,
-    protocol: []const u8,
-    headers: Headers,
-    body: []const u8,
+    protocol: []const u8 = "HTTP/1.1",
+    headers: Headers = Headers.init(std.heap.page_allocator),
+    body: []const u8 = "",
 
-    pub fn parse(stream: []u8) !Request {
-        var it = std.mem.splitSequence(u8, stream, "\n");
+    pub fn init() !*Request {
+        const request = try std.heap.page_allocator.create(Request);
+        return request;
+    }
+
+    pub fn parse(stream: []u8) !*Request {
+        var it = std.mem.splitSequence(u8, stream, "\r\n");
         const firstLine = it.next() orelse return error.SomeError;
         var firstLineIt = std.mem.splitSequence(u8, firstLine, " ");
         const methodString = firstLineIt.next() orelse return error.SomeError;
@@ -31,35 +172,42 @@ pub const Request = struct {
 
         const body = it.rest();
 
-        const request: Request = .{ .method = method, .target = target, .protocol = protocol, .headers = headers, .body = body };
+        const request = try std.heap.page_allocator.create(Request);
+        request.* = .{ .method = method, .target = target, .protocol = protocol, .headers = headers, .body = body };
         return request;
     }
 
     pub fn format(self: Request, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
         const methodString = std.enums.tagName(Method, self.method) orelse std.debug.panic("unable to parse method to string...", .{});
-        try writer.print("{s} {s} {s}\n", .{ methodString, self.target, self.protocol });
+        try writer.print("{s} {s} {s}\r\n", .{ methodString, self.target, self.protocol });
         var it = self.headers.iterator();
         while (it.next()) |header| {
-            try writer.print("{s}: {s}\n", .{ header.key_ptr.*, header.value_ptr.* });
+            try writer.print("{s}: {s}\r\n", .{ header.key_ptr.*, header.value_ptr.* });
         }
-        try writer.writeAll("\n");
+        try writer.writeAll("\r\n");
         try writer.writeAll(self.body);
     }
 
     pub fn deinit(self: *Request) void {
         self.headers.deinit();
+        std.heap.page_allocator.destroy(self);
     }
 };
 
 pub const Response = struct {
-    protocol: []const u8,
+    protocol: []const u8 = "HTTP/1.1",
     statusCode: u16,
     statusText: []const u8,
-    headers: Headers,
-    body: []const u8,
+    headers: Headers = Headers.init(std.heap.page_allocator),
+    body: []const u8 = "",
 
-    pub fn parse(stream: []u8) !Response {
-        var it = std.mem.splitSequence(u8, stream, "\n");
+    pub fn init() !*Response {
+        const response = try std.heap.page_allocator.create(Response);
+        return response;
+    }
+
+    pub fn parse(stream: []u8) !*Response {
+        var it = std.mem.splitSequence(u8, stream, "\r\n");
         const firstLine = it.next() orelse return error.SomeError;
         var firstLineIt = std.mem.splitSequence(u8, firstLine, " ");
         const protocol = firstLineIt.next() orelse return error.SomeError;
@@ -78,21 +226,30 @@ pub const Response = struct {
 
         const body = it.rest();
 
-        const response: Response = .{ .protocol = protocol, .statusCode = statusCode, .statusText = statusText, .headers = headers, .body = body };
+        if (headers.get("Content-Length") == null) {
+            try headers.put("Content-Length", std.mem.asBytes(&body.len));
+        }
+
+        const response = try std.heap.page_allocator.create(Response);
+        response.* = .{ .protocol = protocol, .statusCode = statusCode, .statusText = statusText, .headers = headers, .body = body };
         return response;
     }
 
     pub fn format(self: Response, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        try writer.print("{s} {} {s}\n", .{ self.protocol, self.statusCode, self.statusText });
+        try writer.print("{s} {} {s}\r\n", .{ self.protocol, self.statusCode, self.statusText });
         var it = self.headers.iterator();
         while (it.next()) |header| {
-            try writer.print("{s}: {s}\n", .{ header.key_ptr.*, header.value_ptr.* });
+            try writer.print("{s}: {s}\r\n", .{ header.key_ptr.*, header.value_ptr.* });
         }
-        try writer.writeAll("\n");
+        if (self.headers.get("Content-Length") == null) {
+            try writer.print("{s}: {}\r\n", .{ "Content-Length", self.body.len });
+        }
+        try writer.writeAll("\r\n");
         try writer.writeAll(self.body);
     }
 
     pub fn deinit(self: *Response) void {
         self.headers.deinit();
+        std.heap.page_allocator.destroy(self);
     }
 };
--- src/server.zig
+++ src/server.zig
@@ -1,52 +1,17 @@
 const std = @import("std");
 const http = @import("http.zig");
 
-pub fn main() !void {
-    var httpServer = try HttpServer.init();
-    defer httpServer.deinit();
-    try httpServer.run();
+pub fn rootHandler(request: *http.Request, response: *http.Response) !void {
+    response.* = .{
+        .statusCode = 200,
+        .statusText = "Ok",
+        .body = request.target,
+    };
 }
 
-const HttpServer = struct {
-    server: std.net.Server,
-
-    pub fn init() !HttpServer {
-        const address = try std.net.Address.parseIp4("127.0.0.1", 8080);
-        const server = try address.listen(.{});
-        const httpServer: HttpServer = .{ .server = server };
-        return httpServer;
-    }
-
-    pub fn deinit(self: *HttpServer) void {
-        self.server.deinit();
-    }
-
-    pub fn run(self: *HttpServer) !void {
-        while (true) {
-            const conn = try self.server.accept();
-            try self.handleConnection(conn);
-        }
-    }
-
-    fn handleConnection(_: *HttpServer, conn: std.net.Server.Connection) !void {
-        defer conn.stream.close();
-        var buffer: [1000]u8 = undefined;
-        for (0..buffer.len) |i| {
-            buffer[i] = 0;
-        }
-        const size = try conn.stream.read(&buffer);
-        var request = try http.Request.parse(buffer[0..size]);
-        std.debug.print("{}\n", .{request});
-        const hardcoded: []const u8 =
-            \\HTTP/1.1 201 Created
-            \\Content-Type: application/json
-            \\Location: http://example.com/users/123
-            \\
-            \\testbody
-        ;
-        const response: []u8 = try std.heap.page_allocator.alloc(u8, hardcoded.len);
-        @memcpy(response, hardcoded);
-        _ = try conn.stream.write(response);
-        defer request.deinit();
-    }
-};
+pub fn main() !void {
+    var httpServer = try http.HttpServer.init(8080);
+    defer httpServer.deinit();
+    try httpServer.route("/", rootHandler);
+    try httpServer.run();
+}
