cbc74ebeced706cfb0eb4cb296298c56aa674d15
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2025-03-16 20:35:47 -0400

    feat: parse startline and headers

--- src/server.zig
+++ src/server.zig
@@ -15,6 +15,46 @@ pub fn handleConnection(conn: std.net.Server.Connection) !void {
     for (0..buffer.len) |i| {
         buffer[i] = 0;
     }
-    _ = try conn.stream.read(&buffer);
-    std.debug.print("{s}\n", .{buffer});
+    const numBytes = try conn.stream.read(&buffer);
+    var request = try parseRequest(buffer[0..numBytes]);
+    defer request.deinit();
+    std.debug.print("Target: {s}\n", .{request.target});
+    std.debug.print("Host: {s}\n", .{request.headers.get("Host") orelse "null"});
 }
+
+pub fn parseRequest(stream: []u8) !Request {
+    var it = std.mem.splitSequence(u8, stream, "\n");
+    const firstLine = it.next() orelse return error.SomeError;
+    var firstLineIt = std.mem.splitSequence(u8, firstLine, " ");
+    const methodString = firstLineIt.next() orelse return error.SomeError;
+    const method = std.meta.stringToEnum(Method, methodString) orelse return error.SomeError;
+    const target = firstLineIt.next() orelse return error.SomeError;
+    const protocol = firstLineIt.next() orelse return error.SomeError;
+
+    var headers = Headers.init(std.heap.page_allocator);
+    while (it.next()) |header| {
+        if (header.len == 0) break;
+        var headerIt = std.mem.splitSequence(u8, header, ": ");
+        const key = headerIt.next() orelse return error.SomeError;
+        const value = headerIt.rest();
+        try headers.put(key, value);
+    }
+
+    const request: Request = .{ .method = method, .target = target, .protocol = protocol, .headers = headers };
+    return request;
+}
+
+const Method = enum { GET, POST, PUT, DELETE };
+
+const Headers = std.StringHashMap([]const u8);
+
+const Request = struct {
+    method: Method,
+    target: []const u8,
+    protocol: []const u8,
+    headers: Headers,
+
+    pub fn deinit(self: *Request) void {
+        self.headers.deinit();
+    }
+};
